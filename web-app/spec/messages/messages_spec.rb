7f802061c01f21ea86e78708645e68eb28404cd8
mikesaurio <mikesaurio@gmail.com>
2017-02-22 14:18:13 -0600

    change method in test sms_response for let(:sms_response)

--- web-app/spec/messages/messages_spec.rb
+++ web-app/spec/messages/messages_spec.rb
@@ -7,6 +7,28 @@ module MessagesModule
   end
 
   context "Message module" do
+      
+    let(:sms_response) {'{"sid": "SMc728c48b3ad54e28bd0e28b94fd60cd6",
+        "date_created": "Wed, 22 Feb 2017 16:40:36 +0000",
+        "date_updated": "Wed, 22 Feb 2017 16:40:36 +0000",
+        "date_sent": null,
+        "account_sid": "ACa392a3db6ddfec3e4c634c3b80a937a4",
+        "to": "+525530265963",
+        "from": "+17066230189",
+        "messaging_service_sid": null,
+        "body": "Sent from your Twilio trial account - Hello World",
+        "status": "queued",
+        "num_segments": "1",
+        "num_media": "0",
+        "direction": "outbound-api",
+        "api_version": "2010-04-01",
+        "price": null,
+        "price_unit": "USD",
+        "error_code": null,
+        "error_message": null,
+        "uri": "/2010-04-01/Accounts/ACa392a3db6ddfec3e4c634c3b80a937a4/Messages/SMc728c48b3ad54e28bd0e28b94fd60cd6.json",
+        "subresource_uris": {"media": "/2010-04-01/Accounts/ACa392a3db6ddfec3e4c634c3b80a937a4/Messages/SMc728c48b3ad54e28bd0e28b94fd60cd6/Media.json"}
+        }'}
 
     it "send sms message" do
       number = "+525530265963"
@@ -16,33 +38,6 @@ module MessagesModule
       allow(client).to receive(:create).with(hash_including(from: '+17066230189', to: number, body: msg)){sms_response}
       expect(client).to receive(:create).with(hash_including(from: '+17066230189', to: number, body: msg))
       MessagesModule.send_SMS_message(number,msg)
-    end
-
-    def sms_response
-        '{
-            "sid": "SMc728c48b3ad54e28bd0e28b94fd60cd6",
-            "date_created": "Wed, 22 Feb 2017 16:40:36 +0000",
-            "date_updated": "Wed, 22 Feb 2017 16:40:36 +0000",
-            "date_sent": null,
-            "account_sid": "ACa392a3db6ddfec3e4c634c3b80a937a4",
-            "to": "+525530265963",
-            "from": "+17066230189",
-            "messaging_service_sid": null,
-            "body": "Sent from your Twilio trial account - Hello World",
-            "status": "queued",
-            "num_segments": "1",
-            "num_media": "0",
-            "direction": "outbound-api",
-            "api_version": "2010-04-01",
-            "price": null,
-            "price_unit": "USD",
-            "error_code": null,
-            "error_message": null,
-            "uri": "/2010-04-01/Accounts/ACa392a3db6ddfec3e4c634c3b80a937a4/Messages/SMc728c48b3ad54e28bd0e28b94fd60cd6.json",
-            "subresource_uris": {
-                "media": "/2010-04-01/Accounts/ACa392a3db6ddfec3e4c634c3b80a937a4/Messages/SMc728c48b3ad54e28bd0e28b94fd60cd6/Media.json"
-            }
-        }'
     end
   end
 end
