4cf31c9ef78fd65a4e069ca12b374738eff9bad4
isabel anguiano <isabel.anguianop@gmail.com>
2017-02-27 19:17:17 +0000

    Add conditional input text for checkbox

--- web-app/app/assets/javascripts/application.js
+++ web-app/app/assets/javascripts/application.js
@@ -15,7 +15,20 @@
 //= require turbolinks
 //= require_tree .
 $(document).ready(function() {
+  //Expander controller
   $('.expander-trigger').click(function(){
     $(this).toggleClass("expander-hidden");
   });
+
+  //Form conditional inputs
+  $(document).on('click', '.js-toggle-required', function() {
+    var currentInput = $(this).parent('.custom-checkbox').siblings('.custom-text-input').children('input')
+    if(currentInput.is(':disabled')) {
+      currentInput.prop('disabled', false)
+      currentInput.siblings('.required-field').toggle(100)
+    }else {
+      currentInput.prop('disabled', true)
+      currentInput.siblings('.required-field').toggle(100)
+    }
+  })
 });
