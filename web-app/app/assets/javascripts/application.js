f4eecf449b0b96afda87c146357c2c413ee936c2
isabel anguiano <isabel.anguianop@gmail.com>
2017-02-27 14:20:02 +0000

    Add functionality to expander div

--- web-app/app/assets/javascripts/application.js
+++ web-app/app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  $('.expander-trigger').click(function(){
+    $(this).toggleClass("expander-hidden");
+  });
+});
