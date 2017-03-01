c3ab12170913097410c243d76cb9f311fd623e00
mikesaurio <mikesaurio@gmail.com>
2017-02-28 21:09:45 -0600

    add coverage and test landing

--- web-app/spec/features/landing_spec.rb
+++ web-app/spec/features/landing_spec.rb
@@ -4,9 +4,19 @@ module NotificationCenter
 
   context "landing module" do
     
-    it "navbar" do
+    it "navbar content" do
       visit root_path
-      expect(page).to have_content "California Alerts" 
+      expect(page).to have_content I18n.t('navbar.residents')
+      expect(page).to have_content I18n.t('navbar.government')
+      expect(page).to have_content I18n.t('navbar.About')
+      expect(page).to have_content "EN ES"
+    end
+
+    it "can create user" do
+      visit root_path
+      find('#create_user_btn', :visible => false).click
+      expect(page).to have_content "Get Started"
+      expect(page).to have_content "Fill this form and get notified when it matters"
     end
   end
 end
