2790562f2ea6f08a00778f92ff8d39ccf7c42b54
Giovanni Cortes <me@giovannicortes.com>
2017-03-01 12:23:48 -0600

    Devops (#58)
    * Created firs implementation of Docker, sidekiq is failing
    
    * Fixed sidekiq connection issues with redis container
    
    * Added application to Dockerfile

--- web-app/config/initializers/sidekiq.rb
+++ web-app/config/initializers/sidekiq.rb
@@ -1,3 +1,9 @@
-if ENV["REDISCLOUD_URL"]
-    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+require 'sidekiq'
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://redis:6379" } }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://redis:6379" } }
 end
