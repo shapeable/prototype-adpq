e903fd55b11878a787ce5bd8b623fb557f2bed27
mikesaurio <mikesaurio@gmail.com>
2017-03-01 17:55:19 -0600

    create simple test user new

--- web-app/spec/features/create_user_spec.rb
+++ web-app/spec/features/create_user_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 module UserAdmin
 
-  context "User module" do
+  context "User module", js: true do
     
-    it "navbar content" do
+    it "create user validations" do
       visit new_user_path
       find('#submit_btn', :visible => false).click
+      expect(page).to have_content I18n.t('user.form.validations.not_settings')
     end
 
   end
