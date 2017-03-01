8724a763c8c0880d30be57dff2bf29fe040b40d0
mikesaurio <mikesaurio@gmail.com>
2017-03-01 10:11:23 -0600

    add codeclimate.yml

--- web-app/spec/spec_helper.rb
+++ web-app/spec/spec_helper.rb
@@ -14,8 +14,8 @@
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'simplecov'
-SimpleCov.start
+require 'coveralls'
+Coveralls.wear!
 
 RSpec.configure do |config|
 
