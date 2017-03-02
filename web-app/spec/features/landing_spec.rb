ae7f0f87ebc7783855d068361719e319886aaaa1
mikesaurio <mikesaurio@gmail.com>
2017-03-02 14:14:19 -0600

    add navbar tests

--- web-app/spec/features/landing_spec.rb
+++ web-app/spec/features/landing_spec.rb
@@ -12,10 +12,29 @@ module LandingCenter
       expect(page).to have_content "EN ES"
     end
 
+    it "navbar links activated" do
+      visit root_path
+      find('#nav_residents', :visible => false).click
+      expect(page).to have_content I18n.t('user.form.title')
+
+      visit root_path
+      find('#nav_about', :visible => false).click
+      expect(page).to have_content I18n.t("landing.description.title")
+
+    end
+
     it "can visit create user" do
       visit root_path
       find('#create_user_btn', :visible => false).click
       expect(page).to have_content I18n.t('user.form.title')
+    end
+
+    it "i18n page", js: true do
+      visit root_path
+      find('#nav_es', :visible => false).click
+      expect(page).to have_content 'Para residentes'
+      find('#nav_en', :visible => false).click
+      expect(page).to have_content 'For residents'
     end
   end
 end
