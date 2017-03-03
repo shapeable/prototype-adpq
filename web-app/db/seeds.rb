c9bbdc00a5ec393270cb998587b5574ca28b37a2
Isabel Anguiano <isabel.anguianop@gmail.com>
2017-03-04 00:56:53 +0900

    Correct typos on landing and seeds (#86)

--- web-app/db/seeds.rb
+++ web-app/db/seeds.rb
@@ -2,7 +2,7 @@ Alarm.delete_all
 AlarmCategory.delete_all
 
 category_1 = AlarmCategory.create(name: 'Real time alerts & Forecast',name_es: "Alertas en tiempo real y pronóstico", description: 'Get informed in advance and prevent risks concerning weather hazards, tsunamis, earthquakes and tornadoes.')
-category_2 = AlarmCategory.create(name: 'Meeting Points & Collection Centers',name_es: "Puntos de reunion y centros de acopio", description: 'Know about safe places for emergency and places for helping those in neeed.')
+category_2 = AlarmCategory.create(name: 'Meeting Points & Collection Centers',name_es: "Puntos de reunion y centros de acopio", description: 'Know about safe places for emergency and places for helping those in need.')
 category_3 = AlarmCategory.create(name: 'Important Broadcast',name_es: "Transmiciones importantes", description: 'Receive live instructions to evacuate or breaking news about a contingency.')
 
 Alarm.create(name: "Flooding Occurring or Imminent",name_es: "Inundación inminente o en proceso", description: "weather", end_point: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Climate_Outlooks/cpc_weather_hazards/MapServer/4?f=pjson", alarm_category_id: category_1.id)
