e516e5af284551d1689d2ba84def72e369c1254d
isabel anguiano <isabel.anguianop@gmail.com>
2017-03-03 02:23:47 +0000

    Update functionality on enrollment checkbox, now hide and show the text input instead of disabling it

--- web-app/app/assets/javascripts/users.js
+++ web-app/app/assets/javascripts/users.js
@@ -1,13 +1,9 @@
 //Form conditional inputs
 $(document).on('click', '.js-toggle-required', function() {
-    var currentInput = $(this).parent('.custom-checkbox').siblings('.custom-text-input').children('input')
-        if(currentInput.is(':disabled')) {
-        currentInput.prop('disabled', false)
-        currentInput.siblings('.required-field').toggle(100)
-        }else {
-        currentInput.prop('disabled', true)
-        currentInput.siblings('.required-field').toggle(100)
-        }
+  var currentInput = $(this).parent('.custom-checkbox').siblings('.custom-text-input')
+  var requiredMark=currentInput.children('input').siblings('.required-field')
+  currentInput.toggle(100)
+  requiredMark.toggle(100)
 });
 
 function validateEmail($email) {
