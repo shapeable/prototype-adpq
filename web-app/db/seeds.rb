c8abb6f0514ec05909345fc26e11615f70863039
Isabel Anguiano <isabel.anguianop@gmail.com>
2017-03-04 03:26:16 +0900

    Text updates (#93)
    * Update text on seeds and locals as requested
    
    * Add links to social networks

--- web-app/db/seeds.rb
+++ web-app/db/seeds.rb
@@ -1,9 +1,10 @@
 Alarm.delete_all
 AlarmCategory.delete_all
 
-category_1 = AlarmCategory.create(name: 'Real time alerts & Forecast',name_es: "Alertas en tiempo real y pronóstico", description: 'Get informed in advance and prevent risks concerning weather hazards, tsunamis, earthquakes and tornadoes.')
-category_2 = AlarmCategory.create(name: 'Meeting Points & Collection Centers',name_es: "Puntos de reunion y centros de acopio", description: 'Know about safe places for emergency and places for helping those in need.')
-category_3 = AlarmCategory.create(name: 'Important Broadcast',name_es: "Transmiciones importantes", description: 'Receive live instructions to evacuate or breaking news about a contingency.')
+category_1 = AlarmCategory.create(name: 'Source: Dept of interior IGEMS ',name_es: "Fuente: Departamento IGEMS", description: 'Get informed in advance and prevent risks concerning weather hazards, tsunamis, earthquakes and tornadoes.')
+category_2 = AlarmCategory.create(name: 'Meeting Points & Donation Centers',name_es: "Puntos de reunión y centros de acopio", description: 'Know about safe places for emergency and places for helping those in neeed.')
+category_3 = AlarmCategory.create(name: 'Important Broadcasts',name_es: "Transmisiones importantes", description: 'Receive live instructions to evacuate or breaking news about a contingency.')
+
 
 Alarm.create(name: "Flooding Occurring or Imminent",name_es: "Inundación inminente o en proceso", description: "weather", end_point: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Climate_Outlooks/cpc_weather_hazards/MapServer/4?f=pjson", alarm_category_id: category_1.id)
 Alarm.create(name: "Severe Weather",name_es: "Clima extremo", description: "weather", end_point: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Climate_Outlooks/cpc_weather_hazards/MapServer/4?f=pjson", alarm_category_id: category_1.id)
@@ -17,5 +18,5 @@ Alarm.create(name: "Hurricane Warning",name_es: "Advertencia de huracan", descri
 Alarm.create(name: "Severe Thunderstorm Warning",name_es: "Advertencia de tormenta eléctrica", description: "general", end_point: "https://igems.doi.gov/arcgis/rest/services/igems_haz/MapServer/12?f=pjson", alarm_category_id: category_1.id)
 
 
-Alarm.create(name: "Meeting points",name_es: "Puntos de reunion", description: "meeting", end_point: "", alarm_category_id: category_2.id)
-Alarm.create(name: "Risks",name_es: "Riesgo", description: "risks", end_point: "", alarm_category_id: category_3.id)
+Alarm.create(name: "Locations near me",name_es: "Ubicaciones cercanas a mí", description: "meeting", end_point: "", alarm_category_id: category_2.id)
+Alarm.create(name: "Relevant information",name_es: "Información relevante", description: "risks", end_point: "", alarm_category_id: category_3.id)
